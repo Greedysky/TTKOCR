6f65c41182d2a42533bc6c63e1d710119f8daf29
greedysky <greedysky@163.com>
2017-10-15 19:27:54 +0800

    Update application translation[215483]

---
+++ TTKLanguage/cn.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>OCRApplication</name>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="14"/>
+        <source>天天下载</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="74"/>
+        <source>图片文字提取</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="231"/>
+        <source>打开文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="253"/>
+        <source>识别</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="275"/>
+        <source>清除</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKWidget/ocrUiKits/ocrapplication.ui" line="298"/>
+        <source>Copyright (C) 2016 - 2017 Greedysky Studio</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QJson::ParserRunnable</name>
+    <message>
+        <location filename="../TTKThirdParty/TTKExtras/qjson/parserrunnable.cpp" line="62"/>
+        <source>An error occurred while parsing json: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../TTKModule/TTKCore/ocrUtilsKits/ocrwidgetutils.cpp" line="151"/>
+        <location filename="../TTKModule/TTKCore/ocrUtilsKits/ocrwidgetutils.cpp" line="166"/>
+        <source>choose a filename to open under</source>
+        <translation>打开文件</translation>
+    </message>
+    <message>
+        <location filename="../TTKModule/TTKCore/ocrUtilsKits/ocrwidgetutils.cpp" line="181"/>
+        <source>choose a filename to save under</source>
+        <translation>保存文件</translation>
+    </message>
+</context>
+</TS>
