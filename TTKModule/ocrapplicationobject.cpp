cfec9ec6f9ebb6ddd41c802472b6a5e5e7fa7c66
Greedysky <greedysky@163.com>
2023-11-16 22:28:55 +0800

    refactor: Optimize ttk number define module[576014]

--- TTKModule/ocrapplicationobject.cpp
+++ TTKModule/ocrapplicationobject.cpp
@@ -32,10 +32,10 @@ OCRApplicationObject *OCRApplicationObject::instance()
 void OCRApplicationObject::windowCloseAnimation()
 {
     m_animation->stop();
-    m_animation->setDuration(MT_S2MS / 2);
+    m_animation->setDuration(TTK_DN_S2MS / 2);
     m_animation->setStartValue(1.0f);
     m_animation->setEndValue(0.0f);
     m_animation->start();
 
-    QTimer::singleShot(MT_S2MS, qApp, SLOT(quit()));
+    QTimer::singleShot(TTK_DN_S2MS, qApp, SLOT(quit()));
 }
