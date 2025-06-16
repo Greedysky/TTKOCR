e1204909c9f2b6ee136bfbd226fad6ffe03d2f8d
Greedysky <greedysky@163.com>
2025-06-16 14:33:08 +0800

    feat: Add deb package building[622101]

--- TTKUtils/deploy/make_desktop.sh
+++ TTKUtils/deploy/make_desktop.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-curpath=$PWD/deploy
-if [ $# -eq 0 ]; then
-  curpath=$PWD
+dirpath=`dirname $0`
+tmp="${dirpath#?}"
+
+if [ "${dirpath%${tmp}}" != "/" ]; then
+  dirpath=$PWD/${dirpath}
 fi
 
-path=$curpath/share/applications
-if [ ! -d $path ]; then
-  mkdir $path
+deskpath=${dirpath}/share/applications
+if [ ! -d ${deskpath} ]; then
+  mkdir -p ${deskpath}
 fi
 
-packpath=$path/TTKOCR.desktop
+packvern=3.1.0.0
+packpath=${deskpath}/TTKOCR.desktop
 userpath=~/.local/share/applications
-iconpath="$curpath/share/pixmaps/ttkocr.png"
-execpath="$curpath/../TTKOCR"
+iconpath="${dirpath}/share/pixmaps/ttkocr.png"
+execpath="${dirpath}/../TTKOCR"
 
 echo -n "[Desktop Entry]
 Type=Application
-Version=3.1.0.0
+Version=${packvern}
 Name=TTKOCR
 Name[zh_CN]=文字识别
 Name[zh_TW]=文字識別
@@ -27,13 +30,13 @@ GenericName[zh_TW]=文字識別
 Comment=TTKOCR
 Comment[zh_CN]=文字识别
 Comment[zh_TW]=文字識別
-Icon=$iconpath
-Exec=$execpath
+Icon=${iconpath}
+Exec=${execpath}
 Terminal=false
 Keywords=multimedia;ocr;
 Categories=ocr;Qt;
 MimeType=
-X-KDE-StartupNotify=false\n" > $packpath
+X-KDE-StartupNotify=false\n" > ${packpath}
 
-cp -rv $packpath $userpath
-chmod +x $userpath
+cp -rv ${packpath} ${userpath}
+chmod +x ${userpath}
